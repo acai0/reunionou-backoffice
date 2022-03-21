b96186b14b8b9701d7c5f12f40a56868968fcf61
acai0 <alicecai08@hotmail.com>
2022-03-21 11:15:56 +0100

    maj

--- BackOffice/lbs_fabrication_service/src/app/routes/routes_td.php
+++
@@ -1,8 +0,0 @@
-<?php
-use \lbs\fab\app\controller\CommandeController;
-use \lbs\fab\app\middleware\Middleware;
-require __DIR__ .'/../controller/CommandeController.php';
-require __DIR__ .'/../Middleware/Middleware.php';
-$app->get('/commandes/{id}[/]', CommandeController::class. ':getCommande')->setName('getCommande')->add(middleware::class. ':putIntoJson');
-$app->get('/commandes[/]', CommandeController::class. ':getAllCommande')->setName('getAllCommande')->add(middleware::class. ':putIntoJson');
-//$app->get('/commands[/]', CommandeController::class. ':getCommandeStatus')->setName('getCommandeStatus')->add(middleware::class. ':putIntoJson')->add(CommandeController::class. ':check');
