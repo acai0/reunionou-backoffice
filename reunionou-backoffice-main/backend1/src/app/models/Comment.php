d1cd43739eb641adf1db688ad711ea68df6463da
acai0 <alicecai08@hotmail.com>
2022-03-23 16:13:13 +0100

    ok

--- reunionou-backoffice-main/backend1/src/app/models/Comment.php
+++ reunionou-backoffice-main/backend1/src/app/models/Comment.php
@@ -8,4 +8,9 @@ class Comment extends \Illuminate\Database\Eloquent\Model
     protected $table      = 'comment';
     protected $primaryKey = 'id';
     public    $timestamps = false;
+
+    public function event(){
+        return $this->belongsTo('reu\back\models\Event', 'id_event');
+    } 
+
 }
